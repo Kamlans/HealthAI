f05fb7779982b1546ea104fd34df0f94e209161e
Kamlans <kamlans28@gmail.com>
2021-03-08 12:43:53 +0530

    commit-2

---
+++ app/src/main/java/com/example/healthai/Client/ClientFragment.java
@@ -0,0 +1,16 @@
+package com.example.healthai.Client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.healthai.R;
+
+public class ClientFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client_fragment);
+    }
+}
